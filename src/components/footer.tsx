a27a08902e429df099a34c5d83d58642b888d638
Altaf Ali <altafkhanredmi@gmail.com>
2024-04-23 10:12:59 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link"
-
 const Footer = () => {
     return (
         <>
             <footer className="bg-[#3F418C] px-4 py-1 flex items-center justify-center">
-                <Link href="./" className="flex title-font font-medium items-center justify-center text-gray-900">
+                <div className="flex title-font font-medium items-center justify-center text-gray-900">
                     <img className='w-[35px]' src="./img/logo.png" alt="logo" />
                     <span className="text-3xl font-bold text-[rgba(255,255,255,0.80)]">HDR</span>
-                </Link>
+                </div>
                 <p className="text-[15px] text-md text-[rgba(255,255,255,0.80)] ml-4 pl-4 border-l-2 border-gray-200">© 2024 HDR —
-                    <Link href="https://github.com/altaftech" className="text-[rgba(255,255,255,0.80)] ml-1" rel="noopener noreferrer" target="_blank">@altaftech</Link>
+                    <a href="https://github.com/altaftech" className="text-[rgba(255,255,255,0.80)] ml-1" rel="noopener noreferrer" target="_blank">@altaftech</a>
                 </p>
             </footer>
         </>
